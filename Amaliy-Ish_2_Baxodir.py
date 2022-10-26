d33a54b80d7c03b2b795853fa52d9e273a65fa92
bahodirjon03 <114206819+bahodirjon03@users.noreply.github.com>
2022-10-26 06:46:40 +0300

    Add files via upload

---
+++ Amaliy-Ish_2_Baxodir.py
@@ -0,0 +1,3 @@
+n=int(input("n= "))
+qoldi=(86400-n)//60
+print(qoldi)
---
+++ Amaliy-Ish_3_Baxodir.py
@@ -0,0 +1,9 @@
+
+a=int(input("a= "))
+b=int(input("b= "))
+c=int(input("c= "))
+if (a**2+b**2)==c**2:
+    print("uchburchak tog'ri burchakli")
+else:
+    print("bunday emas")
+
