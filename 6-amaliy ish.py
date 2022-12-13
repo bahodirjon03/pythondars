57d398ee72e8445d2c144c031348d5f347be7f0e
bahodirjon03 <114206819+bahodirjon03@users.noreply.github.com>
2022-12-13 20:14:02 +0500

    Add files via upload

--- 6-amaliy ish.py
+++ 6-amaliy ish.py
@@ -1,5 +1,11 @@
 text = "man bugun maktabga keldim va o'qishni boshladim va yaxshi baxolar oldim"
 new = text.split(" ")
+# print(new)
 string=new[1:-1]
 for n in string:
     print(n,end=' ')
+#
+s=0
+for m in new:
+   s+=1
+print("\n",s,"ta so'z bor asl ro'yxatta ")
